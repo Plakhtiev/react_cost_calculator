4537015948879798364d109fb0ee50962ad11d83
Plakhtiev Jaroslav <63554869+Plakhtiev@users.noreply.github.com>
2020-08-13 21:10:52 +0300

    Add components

---
+++ src/componets/history/History.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import HistoryItem from './HistoryItem';
+
+function History() {
+    return (
+        <section className="history">
+            <h3>История расходов!</h3>
+            <ul className="history__list">
+                <HistoryItem />
+
+            </ul>
+        </section>
+    )
+};
+export default History;
