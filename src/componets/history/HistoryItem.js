8f2bec0271bdaeacf3cd140da705e03d14bb8e21
Plakhtiev <monatkotata@gmail.com>
2020-08-13 21:04:59 +0300

    add components

---
+++ src/componets/history/HistoryItem.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function HistoryItem() {
+    return (
+        <li className="history__item history__item-plus">Получил зарплату
+            <span className="history__money">+30000 ₽</span>
+            <button className="history__delete">x</button>
+        </li>
+
+    )
+
+};
+
+export default HistoryItem;
+
+{/* <li className="history__item  history__item-minus">Отдал долг
+<span className="history__money">-10000 ₽</span>
+            <button className="history__delete">x</button>
+        </li> */}
